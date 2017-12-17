8979c9f76739f47196f59efecfb42e8ff6756b64
Will Zhao <ibestwill@gmail.com>
2017-12-17 00:05:45 -0400

    updated chart.min.js file

--- shop_dongtai.php
+++ shop_dongtai.php
@@ -228,7 +228,7 @@ include("includes/footer.php");
 
 <script src="js/bootstrap.min.js"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<script src="js/Chart.min.js"></script>
 
 
 
